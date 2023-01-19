8e36847cf2d575662a9a7da19d8e77ff2fe7e146
ElverGalarga0 <121035810+ElverGalarga0@users.noreply.github.com>
2023-01-19 12:52:40 +0100

    Added Sacrifice Saved Score

--- My project (1)/Assets/SacrificeSaveScore.cs
+++ My project (1)/Assets/SacrificeSaveScore.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public static int scoreValue = 0;
+    Text score;
     // Start is called before the first frame update
     void Start()
     {
-
+        score = GetComponent<Text> ();
     }
 
     // Update is called once per frame
