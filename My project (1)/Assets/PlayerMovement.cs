2e8ba1fffb6ff1a32c849900a14a9bcc2fa87c03
Kaolhuvud <121035620+Kaolhuvud@users.noreply.github.com>
2023-01-19 10:58:36 +0100

    Update PlayerMovement.cs

--- My project (1)/Assets/PlayerMovement.cs
+++ My project (1)/Assets/PlayerMovement.cs
@@ -8,22 +8,22 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
-    //public Animator animator;
+    public Animator animator;
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal"); //Om vi kan gå höger och vänster med WASD
         movement.y = Input.GetAxisRaw("Vertical"); //Om vi kan gå upp och ner med WASD
 
-        /*animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);*/
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
 
         if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            /*animator.SetFloat("Last_Horizontal", Input.GetAxisRaw("Horizontal"));
-            animator.SetFloat("Last_Vertical", Input.GetAxisRaw("Vertical"));*/
+            animator.SetFloat("Last_Horizontal", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("Last_Vertical", Input.GetAxisRaw("Vertical"));
         }
     }
 
